ee2e2e942c51e72fba2679fec628f8bdae1d5b59
Your Name <vinaythadaka.ms@gmail.com>
2024-04-05 09:23:39 -0500

    _Notification got updated

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -44,10 +44,10 @@ namespace BulkyWeb.Controllers
                 ModelState.AddModelError("name", "The Display order cannot exactly match the name.");
             }
 
-            //if (obj.Name != null &&  obj.Name.ToLower() == "test")
-            //{
-            //    ModelState.AddModelError("", "Test is an invalid value");
-            //}
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
 
             // ur adding a condition if is valid then send to db
             if (ModelState.IsValid)
@@ -56,11 +56,103 @@ namespace BulkyWeb.Controllers
 
                 _db.Categories.Add(obj);           // This is for creating like inseting  it also makes all the changes
                 _db.SaveChanges(); // this is used to creste in db
+
+               // used for creating temp data to display message
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
                 return View();
             
             
         }
+
+        // Creating for Edit Page
+        public IActionResult Edit(int? id)
+        {
+            
+            // Here it will check the id from Bulky datababes Category Column id 
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // Here [_db.Categories] means line 28 we have used for select * from db here we used to find only coliumn id 
+            Category? categoryFromDb = _db.Categories.Find(id);// this id u need to pass in edit view page in html // find only uses primary key
+          
+            // 2 other approaches
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj) // Here ur creating again view for post and ur passing the categeory view obj
+        {
+            
+
+            // ur adding a condition if is valid then send to db
+            if (ModelState.IsValid)
+            {
+
+
+                /*_db.Categories.Add(obj);  */         // This is for creating like inseting  it also makes all the changes
+                _db.Categories.Update(obj); // here we use update it will update that category
+                _db.SaveChanges(); // this is used to creste in db
+
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
+        // Deleting
+
+        public IActionResult Delete(int? id)
+        {
+
+            // Here it will check the id from Bulky datababes Category Column id 
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // Here [_db.Categories] means line 28 we have used for select * from db here we used to find only coliumn id 
+            Category? categoryFromDb = _db.Categories.Find(id);// this id u need to pass in edit view page in html // find only uses primary key
+
+            // 2 other approaches
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id) // Here ur creating again view for post and ur passing the categeory view obj
+        {
+            // first u need to find category from db
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+
+           _db.SaveChanges(); // this is used to creste in db
+            TempData["success"] = "Category deleted Successfully";
+            return RedirectToAction("Index");
+            
+        }
+        
     }
 }
